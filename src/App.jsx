18040af77bb8dec975b8bb28a76f509589d200c0
metalekan <metadelekan@gmail.com>
2024-11-08 20:23:17 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Outlet } from "react-router";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <Button variant="outline">Button</Button>
+    </div>
+  );
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Register from "./pages/Auth/Register";
 
-const router = createBrowserRouter([
-  {
-      path: "/",
-      element: <Any />,
-  }
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/register" element={<Register />} />
+    </Route>
+  )
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
